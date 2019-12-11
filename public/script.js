fe59cb29b8322fcc260c49eb5f35301cef1b231b
Jmtri7 <Jmtri7@gmail.com>
2019-12-10 19:16:39 -0500

    Smooth front end. Lacking features besides filter.

--- public/script.js
+++ public/script.js
@@ -1,12 +1,18 @@
+var filterText = '';
+$("#filterField").keyup(function(){
+	filterText = $("#filterField").val();
+	getFlowers();
+});
+
 getFlowers();
 
 function getFlowers() {
-	$.get('/flowers', function(data) {
+	$.get("/flowers/" + filterText, function(data) {
 		if(!data) {
 			console.log("Flowers not found!");
 		}
 		for(var i = 0; i < data.length; i++) {
-			console.log(data[i].name);
+			//console.log(data[i].name);
 		}
 		showFlowers(data);
 	});
@@ -14,22 +20,23 @@ function getFlowers() {
 
 function showFlowers(flowers) {
 	var flowersSection = document.getElementById("flowers");
-	console.log(flowers[0])
+	$(flowersSection).empty();
+	var decksize = 3;
 	for(var i = 0; i < flowers.length; i++) {
-		if(i % 3 == 0) {
+		if(i % decksize == 0) {
 			var deck = document.createElement("div");
 			deck.className = "card-deck spacer";
 			flowersSection.appendChild(deck);
 		}
 		var card = document.createElement("div");
 		card.className = "card";
-		card.style = "width: 18rem;"
+
 		var cardimg = document.createElement("img");
 		cardimg.className="card-img-top";
-		cardimg.src="flower.jpg";
+		cardimg.src="img/flower.jpg";
 		cardimg.alt="Card image cap";
 		var cardbody = document.createElement("div");
-		cardbody.className="card-body";
+		cardbody.className="card-body d-flex flex-column";
 		var cardtitle = document.createElement("h5");
 		cardtitle.className="card-title";
 		cardtitle.innerHTML = flowers[i].COMNAME;
@@ -37,9 +44,9 @@ function showFlowers(flowers) {
 		cardtext.className="card-text";
 		cardtext.innerHTML = flowers[i].GENUS + " " + flowers[i].SPECIES;
 		var cardbutton = document.createElement("a");
-		cardbutton.href="#";
-		cardbutton.className="btn btn-primary";
-		cardbutton.innerHTML = "PICK ME";
+		cardbutton.href="flowers/" + (flowers[i].COMNAME).replace(' ', "\%20");
+		cardbutton.className="btn btn-primary mt-auto";
+		cardbutton.innerHTML = "Pick Me";
 		card.appendChild(cardimg);
 		card.appendChild(cardbody);
 		cardbody.appendChild(cardtitle);
@@ -47,16 +54,21 @@ function showFlowers(flowers) {
 		cardbody.appendChild(cardbutton);
 		deck.appendChild(card);
 	}
-	if(flowers.length % 3 == 2) {
-		var card = document.createElement("div");
-		card.className = "card";
-		card.style = "width: 18rem;"
-		deck.appendChild(card);
+	if(flowers.length % decksize != 0) {
+		for(var i = 1; i <= decksize - (flowers.length % decksize); i++) {
+			var card = document.createElement("div");
+			card.className = "card";
+			card.style = "width: 18rem;"
+			deck.appendChild(card);
+		}
 	}
-	if(flowers.length % 3 == 1) {
-		var card = document.createElement("div");
-		card.className = "card";
-		card.style = "width: 18rem;"
-		deck.appendChild(card);
-	}
+
+	
+	$(".card").click(this, function() {
+		var comname = $(this).children(".card-body").children(".card-title").prop("innerHTML");
+		//console.log(comname);
+		$("#filterField").val(comname);
+		filterText = comname;
+		getFlowers();
+	});
 }
--- server.js
+++ server.js
@@ -6,11 +6,6 @@ var db = new sqlite3.Database('db/flowers2019.db');
 
 app.use(express.static(__dirname + '/public'));
 
-// home page
-app.get('/', function(request, response) {
-	response.sendFile('public/app.html', {root: __dirname })
-});
-
 // display all flowers
 app.get('/flowers/', function(request, response) {
 	db.all('SELECT * FROM flowers', function(err, rows) {
@@ -25,7 +20,7 @@ app.get('/flowers/', function(request, response) {
 
 // filter flowers
 app.get('/flowers/*', function(request, response) {
-	db.all('SELECT * FROM flowers WHERE comname LIKE \'%' + request.url.substring(request.url.lastIndexOf('/') + 1) + '%\';', function(err, rows) {
+	db.all('SELECT * FROM flowers WHERE comname LIKE \'%' + (request.url.substring(request.url.lastIndexOf('/') + 1)).replace(/%20/g, ' ') + '%\';', function(err, rows) {
 		if(err) {
 			console.log("Error: " + err);
 		}
@@ -35,6 +30,24 @@ app.get('/flowers/*', function(request, response) {
 	});
 });
 
+// filter sightings
+app.get('/sightings/*', function(request, response) {
+	db.all('SELECT * FROM sightings, flowers WHERE comname = name AND comname LIKE \'%' + (request.url.substring(request.url.lastIndexOf('/') + 1)).replace(/%20/g, ' ') + '%\';', function(err, rows) {
+		if(err) {
+			console.log("Error: " + err);
+		}
+		else {
+			response.send(rows);
+		}
+	});
+});
+
+// home page fail safe
+app.get('/*', function(request, response) {
+	response.sendFile('public/app.html', {root: __dirname })
+});
+
+// anticipate a reason to post UNUSED
 app.post('/', function(request, response){
 	response.send('My App');
 });
